757d0d8fe03763f99897fb428b63d3f965c23ebb
Alejandro S <alejandrosanudo10@gmail.com>
2024-07-10 23:47:55 +0200

    Changed text

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -567,7 +567,7 @@ export default function Home() {
                                     <path strokeLinecap="round" strokeLinejoin="round"
                                           d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z"/>
                                 </svg>
-                                More repositories
+                                More projects
                             </a>
                         </Glow>
                     </GlowCapture>
